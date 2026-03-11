81dfcfa1f2b067df51ba8b7283295c537d2a7d4b
ArunArdor <abatta@una.edu>
2026-03-10 23:23:06 -0500

    DONE

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-//app.js
-//we are in ES6, use this. 
 import 'dotenv/config';
 import dns from 'node:dns';
 dns.setServers(['1.1.1.1', '8.8.8.8']);
@@ -7,28 +5,19 @@ dns.setServers(['1.1.1.1', '8.8.8.8']);
 import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-import { readFile } from 'fs/promises';  // For async file reading
 import mongoose from 'mongoose';
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-//const { MongoClient, ServerApiVersion } = require('mongodb');
-
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 const PORT = process.env.PORT || 3000;
-
 
 app.use(express.static(join(__dirname, 'public')));
 app.use(express.json());
 
 async function run() {
   try {
-    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
     await mongoose.connect(process.env.MONGO_URI, { dbName: 'fantastic' });
-
     console.log("Connected to MongoDB");
     console.log("Database: ", mongoose.connection.name);
   } catch (err) {
@@ -38,58 +27,15 @@ async function run() {
 }
 run().catch(console.dir);
 
-// MongoDB schema (structure of data in database)
 const attendeeSchema = new mongoose.Schema({
   name: String,
   status: String
 });
 
-// MongoDB model
 const Attendee = mongoose.model("Attendee", attendeeSchema);
 
-
-// middlewares aka endpoints aka 'get to slash' {http verb} to slash {you name ur endpoint}
 app.get('/', (req, res) => {
-  // res.send('Hello Express'); //string response
-  // res.sendFile('index.html'); // <- this don't work w/o imports, assign, and arguements
   res.sendFile(join(__dirname, 'public', 'index.html'));
-
-})
-
-app.get('/inject', (req, res) => {
-  // Inject a server variable into barry.html: templating view like ejs or pug
-  readFile(join(__dirname, 'public', 'index.html'), 'utf8')
-    .then(html => {
-      // Replace a placeholder in the HTML (e.g., {{myVar}})
-      const injectedHtml = html.replace('{{myVar}}', myVar);
-      res.send(injectedHtml);
-    })
-    .catch(err => {
-      res.status(500).send('Error loading page');
-    });
-})
-
-app.get('/api/json', (req, res) => {
-  const myVar = 'Hello from server!';
-  res.json({ myVar });
-})
-
-app.get('/api/query', (req, res) => {
-  console.log("client request with query param:", req.query.name);
-  res.json({ "message": req.query.name });
-});
-
-app.get('/api/url/:iaddasfsd', (req, res) => {
-
-  console.log("client request with URL param:", req.params.iaddasfsd);
-  res.json({ "message": `Hi, ${req.params.iaddasfsd}. How are you?` });
-});
-
-
-app.post('/api/body', (req, res) => {
-  // console.log("the body:", req.body); 
-  console.log("client request with body:", req.body.name);
-  res.json({ "message": req.body.name });
 });
 
 app.post('/api/attendees', async (req, res) => {
@@ -98,7 +44,6 @@ app.post('/api/attendees', async (req, res) => {
       name: req.body.name,
       status: req.body.status || "present"
     });
-
     res.status(201).json(attendee);
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -149,7 +94,6 @@ app.delete('/api/attendees/:id', async (req, res) => {
   }
 });
 
-//start the server. 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
